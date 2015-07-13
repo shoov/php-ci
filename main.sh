bd94b6a8d1076009b1c5830a95ac48d546354e87
Amitai Burstein <amitai@gizra.com>
2015-07-13 23:44:26 +0300

    Get .shoov.json

--- main.sh
+++ main.sh
@@ -12,6 +12,9 @@ BRANCH=$(echo $BUILD_INFO | jq '.branch' | cut -d '"' -f 2)
 PRIVATE_KEY=$(echo $BUILD_INFO | jq '.private_key' | cut -d '"' -f 2)
 GITHUB_ACCESS_TOKEN=$(echo $BUILD_INFO | jq '.github_access_token' | cut -d '"' -f 2)
 
+# Get .shoov.json
+curl -o ~/.shoov.json https://live-shoov.pantheon.io/api/v1.0/config?access_token=$ACCESS_TOKEN
+
 # Get GitHub access token
 cd ~/build
 
